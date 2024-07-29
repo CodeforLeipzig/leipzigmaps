358ed1e8c8c53dd3028c961fc498d22ecf31b3dd
Jörg Reichert <joergreichert@users.noreply.github.com>
2024-07-29 08:51:09 +0200

    assemble

--- src/scrapers/leipzig-kivan.js
+++ src/scrapers/leipzig-kivan.js
@@ -30,11 +30,20 @@ const getDetailsHtml = async (id) => {
 const convertToCsv = (objArray) => {
     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
     var str = '';
+    var keys = Object.keys(array[0]);
+    var headline = '';
+    for (var key of keys) {
+        if (headline != '') {
+            headline += ';'
+        }
+        headline += key;
+    }
+    str += headline + '\n';
     for (var i=0; i<array.length; i++) {
         var line = '';
         for (var index in array[i]) {
             if (line != '') {
-                line += ','
+                line += ';'
             }
             line += array[i][index];
         }
@@ -125,10 +134,14 @@ const readJavascriptRendered = async (id) => {
         await page.waitForSelector('#anpsrechpartner-view');
         const ansprechpartnerData = await page.evaluate(() => document.querySelector('#anpsrechpartner-view').innerText);
         fs.writeFileSync(`./public/data-raw/kivan/${id}-ansprechpartner.json`, ansprechpartnerData, 'utf-8');
+    } catch(e) {
+        console.log(e.message);
+    }
+    try {
         const traegerData = await page.evaluate(() => document.querySelector('#traeger-view').innerText);
         fs.writeFileSync(`./public/data-raw/kivan/${id}-traeger.json`, traegerData, 'utf-8');
     } catch(e) {
-        console.log(e);
+        console.log(e.message);
     }
     await browser.close();
 }
@@ -137,7 +150,10 @@ const fillLiteral = ({ str, tokens, fields}) => {
     const literal = {};
     let part = str;
     for (let [index, token] of tokens.entries()) {
-        const tokenIndex = part ? part.indexOf(token) : -1;
+        var tokenIndex = part ? part.indexOf(token) : -1;
+        if (tokenIndex < 0 && token == "Ansprechpartner") {
+            tokenIndex = part ? part.indexOf("Tagespflegeperson") : -1;
+        }
         if (tokenIndex >= 0) {
             var value = part.substring(tokenIndex + token.length).trim()
             literal[fields[index]] = value;
@@ -155,14 +171,12 @@ const parseAnsprechpartnerConfigured = ({str, prefix}) => {
         "E-Mail : "
     ].reverse();
     const ansprechpartnerPrefix = `${prefix}Ansprechpartner`
-    const fields = [ "Email", "Fax", "Telefonnummer", "Nachname" ].map(field => `${ansprechpartnerPrefix}${field}`)
+    const fields = [ "Email", "Faxnummer", "Telefonnummer", "Nachname" ].map(field => `${ansprechpartnerPrefix}${field}`)
     const ansprechpartner = fillLiteral({ str, tokens, fields});
-    console.log(JSON.stringify(ansprechpartner, null, 2));
     const nameParts = ansprechpartner[`${ansprechpartnerPrefix}Nachname`].split(" ");    
     ansprechpartner[`${ansprechpartnerPrefix}Anrede`] = nameParts.length > 0 ? nameParts[0].trim() : null
     ansprechpartner[`${ansprechpartnerPrefix}Vorname`] = nameParts.length > 2 ? nameParts.slice(1, -1).join(" ").trim() : null
     ansprechpartner[`${ansprechpartnerPrefix}Nachname`] = nameParts.length > 0 ? nameParts[nameParts.length-1].trim() : null
-    console.log(JSON.stringify(ansprechpartner, null, 2));
     return ansprechpartner;
 }
 
@@ -174,7 +188,6 @@ const parseTraeger = (str) => {
         traegerName: initialLiteral.traegerName,
         ...parseAnsprechpartnerConfigured({str: "Ansprechpartner " + initialLiteral.traegerAnsprechpartnerNachname, prefix: "traeger"})
     };
-    console.log(JSON.stringify(traeger, null, 2));
     return traeger;
 }
 
@@ -190,41 +203,112 @@ const readDetailsJson = async (einrichtungId) => {
     const htmlUrl = `https://www.meinkitaplatz-leipzig.de/app/de/einrichtung/show/${einrichtungId}?processid=&index=0&returnurl=%2Fde%2Feinrichtung%3Freturnurl%3D`
     await page.goto(htmlUrl);
     await page.setViewport({width: 1080, height: 1024});
-    console.log(`ident: ${einrichtungId}`)
-    const evaluateFun = (ident) => {
-
-        console.log(`ident2: ${ident}`)
-//        const response = await fetch(`https://www.meinkitaplatz-leipzig.de/api/einrichtung/show/${ident}`)
-//        const data = await response.json();
-//        console.log(data);
-//        return data;
-        return null;
+    const evaluateFun = async (ident) => {
+        const response = await fetch(`https://www.meinkitaplatz-leipzig.de/api/einrichtung/show/${ident}`)
+        return response;    
     }
 
-    const response = await page.evaluate(evaluateFun, einrichtungId);
+    const data = await page.evaluate(evaluateFun, einrichtungId);
     await browser.close();
-
-    return response;
+    fs.writeFileSync(`./public/data-raw/kivan/${einrichtungId}.json`, JSON.stringify(data, null, 2), 'utf-8');
+    return data;
 }
 
 const downloadDetailsData = () => {
     const content = fs.readFileSync('./public/data-raw/leipzig-kivan.json', 'utf-8');
     const jsonData = JSON.parse(content);
     var c = 0;
-    const filtered = jsonData.filter(elem => !fs.existsSync(`./public/data-raw/kivan/${elem.einrichtungsId}-ansprechpartner.json`));
+    const filtered = jsonData.filter(
+        elem => !fs.existsSync(`./public/data-raw/kivan/${elem.einrichtungsId}-ansprechpartner.json`) 
+        || !fs.existsSync(`./public/data-raw/kivan/${elem.einrichtungsId}-traeger.json`)
+    );
     var interval = setInterval(function() {
         const elem = filtered[c];
         const ident = elem.einrichtungsId; 
-        console.log(`handle ident ${ident}`);
+        console.log(ident);
         readJavascriptRendered(ident)
         c++; 
         if(c >= filtered.length) clearInterval(interval);
     }, 3000);        
 }
 
+const createJson = () => {
+    const content = fs.readFileSync('./public/data-raw/leipzig-kivan.json', 'utf-8');
+    const jsonData = JSON.parse(content);
+    const newJsonData = [];
+    jsonData.forEach((elem) => {
+        const einrichtungsId = elem["einrichtungsId"];
+        try {
+            var einrichtungAnsprechpartner = {};
+            try {
+                const detailsAnsprechpartnerData = fs.readFileSync(`./public/data-raw/kivan/${einrichtungsId}-ansprechpartner.json`, 'utf-8');
+                einrichtungAnsprechpartner = parseAnsprechpartnerConfigured({str: detailsAnsprechpartnerData, prefix: "einrichtung"});
+            } catch(e) {
+            }
+
+            const detailsTraegerData = fs.readFileSync(`./public/data-raw/kivan/${einrichtungsId}-traeger.json`, 'utf-8');        
+            const traeger = parseTraeger(detailsTraegerData)
+
+            const traegerAdresseStr = traeger["traegerName"].replace(elem["traegerName"], "")
+            const plzIndex = traegerAdresseStr.indexOf(" 0");
+            const traegerAdresse = {};
+            if (plzIndex > 1) {
+                const plzCity = traegerAdresseStr.substring(plzIndex+1);
+                const strasseHnr = traegerAdresseStr.substring(0, plzIndex);
+                const plzIndex2 = plzCity.indexOf(" ")
+                traegerAdresse["plz"] = plzCity.substring(0, plzIndex2);
+                traegerAdresse["city"] = plzCity.substring(plzIndex2+1);
+                const parts = strasseHnr.split(" ");
+                traegerAdresse["hausNr"] = parts[parts.length-1];
+                traegerAdresse["strasse"] = parts.splice(parts.length-2, 1).join(" ");
+            }    
+            
+            const entry = {
+                "einrichtungId": elem["einrichtungsId"],
+                "einrichtungName": elem["einrichtungName"],
+                "einrichtungArt": elem["einrichtungsart"],
+                "einrichtungArtText": elem["einrichtungsartText"],
+                "einrichtungStrasse": elem["einrichtungStrasse"],
+                "einrichtungHausnummer": elem["einrichtungHausnummer"],
+                "einrichtungPostleitzahl": elem["einrichtungPostleitzahl"],
+                "einrichtungOrt": elem["einrichtungOrt"],
+                "einrichtungUrl": elem["einrichtungUrl"],
+                "einrichtungLatitude": elem["einrichtungLatitude"],
+                "einrichtungLongitude": elem["einrichtungLongitude"],
+                "einrichtungAnsprechpartnerAnrede": elem["ansprechpartnerAnrede"] || einrichtungAnsprechpartner["einrichtungAnsprechpartnerAnrede"],
+                "einrichtungAnsprechpartnerVorname": elem["ansprechpartnerVorname"] || einrichtungAnsprechpartner["einrichtungAnsprechpartnerVorname"],
+                "einrichtungAnsprechpartnerNachname": elem["ansprechpartnerNachname"] || einrichtungAnsprechpartner["einrichtungAnsprechpartnerNachname"],
+                "einrichtungAnsprechpartnerTelefonnummer": elem["ansprechpartnerTelefonnummer"] || einrichtungAnsprechpartner["einrichtungAnsprechpartnerTelefonnummer"],
+                "einrichtungAnsprechpartnerFaxnummer": elem["ansprechpartnerFaxnummer"] || einrichtungAnsprechpartner["einrichtungAnsprechpartnerFaxnummer"],
+                "einrichtungAnsprechpartnerEmail": elem["ansprechpartnerEmail"] || einrichtungAnsprechpartner["einrichtungAnsprechpartnerEmail"],
+                "traegerName": elem["traegerName"],
+                "traegerAnsprechpartnerAnrede": traeger["traegerAnsprechpartnerAnrede"],
+                "traegerAnsprechpartnerVorname": traeger["traegerAnsprechpartnerVorname"],
+                "traegerAnsprechpartnerNachname": traeger["traegerAnsprechpartnerNachname"],
+                "traegerAnsprechpartnerTelefonnummer": traeger["traegerAnsprechpartnerTelefonnummer"],
+                "traegerAnsprechpartnerFaxnummer": traeger["traegerAnsprechpartnerFaxnummer"],
+                "traegerAnsprechpartnerEmail": traeger["traegerAnsprechpartnerEmail"],
+                "traegerStrasse": traegerAdresse["strasse"],
+                "traegerHausnummer": traegerAdresse["hausNr"],
+                "traegerPostleitzahl": traegerAdresse["plz"],
+                "traegerOrt": traegerAdresse["city"],
+            }
+            newJsonData.push(entry);
+        } catch(e) {
+            console.log("error for " + einrichtungsId + ": " + e.message)
+        }
+    });
+    const newContent = JSON.stringify(newJsonData, null, 2);
+    //console.log(JSON.stringify(newJsonData, null, 2));
+    fs.writeFileSync('./public/data/leipzig-kivan.json', newContent, 'utf-8');
+    const csv = convertToCsv(newContent.replace(";", ", "))
+    fs.writeFileSync('./public/data/leipzig-kivan.csv', csv, 'utf-8');
+}
+
 //readData();
 //parseAnsprechpartner();
-//readDetailsJson(177);
+//readDetailsJson(1057);
 //parseTraeger();
 //readDataRaw();
-//downloadDetailsData
+//downloadDetailsData();
+createJson()
